41f8426df26754c244030ba74b2af5969d43704b
panwankun <panwankun@qiyi.com>
2020-12-20 08:38:58 +0800

    first commit

---
+++ app/src/main/java/com/panda/free/card/launcher/MainActivity.java
@@ -0,0 +1,16 @@
+package com.panda.free.card.launcher;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+/**
+ * Created by mPaaS on 16/9/28.
+ */
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.panda.free.card.launcher.R.layout.main);
+    }
+}
